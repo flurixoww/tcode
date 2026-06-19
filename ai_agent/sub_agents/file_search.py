b841de0a35a7f482ce1999bc5c1ccbd1b61bfe60
Dominik Jaskuc <flurixoww@gmail.com>
2026-06-19 12:03:12 +0200

    Code structure improvment & logic fix

--- ai_agent/sub_agents/file_search.py
+++ ai_agent/sub_agents/file_search.py
@@ -4,45 +4,121 @@ import os
 import chromadb
 
 
-# Algorithm for extracting file's name and content
-def file_info():
-    files = []
-    files_content = []
+def file_info() -> tuple:
+    """
+    Extracts the names and the content of the files in the current directory.
 
-    for file in os.listdir():
-        if os.path.isfile(file):
-            files.append(file)
-            with open(file, "r", encoding="utf-8") as f:
-                file_content = f.read()
-                files_content.append(file_content)
-    return files, files_content
+    Args:
+        None
+
+    Returns:
+        tuple: A tuple containing two lists:
+            files: The list containing names of the available files in the current directory.
+            files_content: The list containing content of the files.
+
+    Raises:
+        RuntimeError: If file receiving was unsuccessful.
+    """
+
+    try:
+        files = []
+        files_content = []
+
+        for file in os.listdir():
+            if os.path.isfile(file):
+                files.append(file)
+                with open(file, "r", encoding="utf-8") as f:
+                    file_content = f.read()
+                    files_content.append(file_content)
+        return files, files_content
+
+    except Exception as e:
+        raise RuntimeError(f"File extraction was unsuccessful: {e}") from e
 
 
-def file_distance(ids, documents, prompt):
-    # Chroma initialization
-    chroma_client = chromadb.Client()
+def chroma_client_initialization(ids: list, documents: list) -> chromadb.Collection:
+    """
+    Initializes chroma and loads documents into the structure
 
-    # Chroma file architecture
-    files_collection = chroma_client.create_collection(name="files")
+    Args:
+        ids (list): The list of possible files.
+        documents (list): The list of content of files.
 
-    # Importing files and content of the files into chroma file architecture
-    files_collection.add(ids=ids, documents=documents)
+    Returns:
+        chromadb.Collection: Prepared collection with the files and their content.
 
-    # Chroma RAG model settings and initialization
-    result = files_collection.query(
-        query_texts=prompt,
-        n_results=3,
-        include=["distances"],
-    )
-    return result["ids"], result["distances"]
+    Raises:
+        RuntimeError: If initialization of the model was unsuccessful.
+    """
+
+    try:
+        # Chroma initialization
+        chroma_client = chromadb.Client()
+
+        # Chroma file architecture
+        files_collection = chroma_client.create_collection(name="files")
+
+        # Importing files and content of the files into chroma file architecture
+        files_collection.add(ids=ids, documents=documents)
+
+        return files_collection
+    except Exception as e:
+        raise RuntimeError(f"The initialization of the model failed {e}") from e
 
 
-# Needs improvment, if distance of all documents is >1.25, we should return 3 most plausible
-# At least 3 documents should be present.
-def likely_files(ids, distances):
+def file_distance(files_collection: chromadb.Collection, prompt: str) -> tuple:
+    """
+    Using RAG model finds the likability of the files to the prompt.
 
-    check = len(ids[0])
+    Args:
+        files_collection (chromadb.Collection): Chroma file architecture with already imported files
+        prompt (str): The user's prompt.
 
-    for file_i in range(check - 1):
-        if distances[0][file_i] < 1.25:
-            return ids[0][file_i]
+    Returns:
+        tuple: A tuple containing two lists:
+            result['ids']: Contains the ids of the most relevant files.
+            result['distances']: Contains the possibility of the file connection
+            to the prompt in order respective to the ids.
+    Raiess:
+        RuntimeError: If model didn't work
+    """
+    try:
+        # Chroma RAG model settings and initialization
+        result = files_collection.query(
+            query_texts=prompt,
+            include=["distances"],
+        )
+        return result["ids"], result["distances"]
+    except Exception as e:
+        raise RuntimeError(f"Error occured when initializing a model. {e}") from e
+
+
+def likely_files(file_ids: list, distances: list) -> list:
+    """
+    Picks the closest files to the prompt.
+
+    Args:
+        file_ids (list): File names.
+        distances (list): File distance to the prompt, with the same order as file ids.
+
+    Returns:
+        list: Name of the files from the least to the biggest distance.
+
+    Raises:
+        RuntimeError: If there was a problem picking a closest file.
+    """
+
+    # To be improved in the future
+    likely_files = []
+    try:
+        for file in range(len(file_ids) - 1):
+            if distances[file] <= 1.25:
+                likely_files.append(file_ids[file])
+            if len(likely_files) < 3 and distances[file] > 1.25:
+                likely_files.append(file_ids[file])
+        return likely_files
+
+    except Exception as e:
+        raise RuntimeError(
+            f"Error occured during picking of the closest files {e}"
+        ) from e
